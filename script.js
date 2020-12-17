93667efe7eebab3686ccdaa9484b8300b3eea3de
Reza Mahendra Surin <reza.surin@gmail.com>
2020-12-18 04:33:30 +0700

    website progress estimation: 50%

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // script html
+
+const navBar = document.querySelector('.navbar')
+window.addEventListener('scroll', hideNav)
+
+function hideNav() {
+  if (window.scrollY > navBar.offsetHeight + 150) {
+    navBar.classList.add('activeHide')
+  } else {
+    navBar.classList.remove('activeHide')
+  }
+}
+
+function formOrder () {
+  // script open form
+}
